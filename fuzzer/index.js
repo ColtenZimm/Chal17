a137cff88bb3c09ba7a6e0be920ebabae0a3c618
ColtenZimm <coltenz00@gmail.com>
2023-08-21 19:01:02 -0500

    cleaned up repo

--- fuzzer/index.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const fuzzer = require('./fuzzer')
-const corpus = fs.readFileSync(path.join(__dirname, "corpus.txt")).toString();
-fuzzer(99999, corpus)
